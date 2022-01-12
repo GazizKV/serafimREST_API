3ee6b83c4d50d8d5a2ab1baac544a676d7897476
gazizkv <valitovgaziz@gmail.com>
2022-01-12 17:56:01 +0500

    Initial commit

---
+++ src/test/java/ru/serafimrest_api/SerafimRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.serafimrest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerafimRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
